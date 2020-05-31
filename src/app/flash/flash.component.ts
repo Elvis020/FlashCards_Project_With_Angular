50ad8b6bc3977bf0f85a932225b6c38a15205848
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-05-31 20:19:12 +0000

    first commit

---
+++ src/app/flash/flash.component.ts
@@ -0,0 +1,24 @@
+import { IFlash } from './flash.model';
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-flash',
+  templateUrl: './flash.component.html',
+  styleUrls: ['./flash.component.css']
+})
+export class FlashComponent implements OnInit {
+  @Input() flash: IFlash = {
+    id: 1,
+    question: 'React To Angular',
+    answer: 'No Reaction',
+    show: false
+  };
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/flash/flash.model.ts
@@ -0,0 +1,8 @@
+
+export interface IFlash {
+  question: string;
+  answer: string;
+  show: boolean;
+  id: number;
+  remembered?: 'correct' | 'incorrect';
+}
